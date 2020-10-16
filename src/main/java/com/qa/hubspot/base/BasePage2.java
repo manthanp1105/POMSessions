db16ae6da945f4fe9bbca9229917c220b55b7b72
Manthan Pandya <manthan@Manthans-MacBook-Pro.local>
2020-10-15 23:05:31 -0400

    Added basepage 2

---
+++ src/main/java/com/qa/hubspot/base/BasePage2.java
@@ -0,0 +1,12 @@
+package com.qa.hubspot.base;
+
+public class BasePage2 {
+	
+	public void case1() {
+		System.out.println("BasePage 2 is created");
+	}
+	public void case2() {
+		System.out.println("BasePage 2 is created again");
+	}
+
+}
